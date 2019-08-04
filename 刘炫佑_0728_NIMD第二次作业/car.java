ec1a21872a836baef5573b9e7b3460a942c820ab
Hacker <47812416+Kyrie827@users.noreply.github.com>
2019-08-04 15:04:17 +0800

    Update car.java

--- 刘炫佑_0728_NIMD第二次作业/car.java
+++ 刘炫佑_0728_NIMD第二次作业/car.java
@@ -1,96 +1,92 @@
-IMPORT JAVA.UTIL.SCANNER;
-IMPORT JAVA.IO.IOEXCEPTION;
+import java.util.Scanner;
+import java.io.IOException;
 
-//CMD要输入CHCP 65001才能将GBK编码转换为UTF-8
-CLASS CAR
+//cmd要输入chcp 65001才能将GBK编码转换为UTF-8
+class Car
 {
-    PUBLIC STATIC VOID MAIN(STRING[] ARGS) {
+    public static void main(String[] args) {
 
         //车辆信息
-        INT[] E={1,2,3,4,5,6,7,8};//车辆序号
-        STRING[] N= {"小轿车", "面包车", "大巴车", "低栏车", "高栏车", "厢式车", "日式皮卡车", "美式皮卡车"};//车辆名称
-        INT[] F= {40,65,180,30,47,80, 60, 60};//租车费用（元/（车·天））
-        DOUBLE[] M= {4,7,20,1.5,2.5,4.5,1,3};//最大载人（人）
-        //DOUBLE[] MH= {1.5,2.5,4.5,1,3};//最大载货（吨）
+        int[] e={1,2,3,4,5,6,7,8};//车辆序号
+        String[] n= {"小轿车", "面包车", "大巴车", "低栏车", "高栏车", "厢式车", "日式皮卡车", "美式皮卡车"};//车辆名称   
+        int[] f= {40,65,180,30,47,80, 60, 60};//租车费用（元/（车·天））
+        double[] m= {4,7,20,1.5,2.5,4.5,1,3};//最大载人（人）
+        //double[] mh= {1.5,2.5,4.5,1,3};//最大载货（吨）
 
         //车辆信息分为载人量和载货量
-        FOR(INT I=0;I<3;I++)
+        for(int i=0;i<3;i++)
         {
-            SYSTEM.OUT.PRINTLN("序号:" + E[I]);
-            SYSTEM.OUT.PRINTLN("车辆名称:" + N[I]);
-            SYSTEM.OUT.PRINTLN("租车费用：" + F[I] + "元/（车·天）");
-            SYSTEM.OUT.PRINTLN("最大载人：" + M[I] + "人");
+            System.out.println("序号:" + e[i]);
+            System.out.println("车辆名称:" + n[i]);
+            System.out.println("租车费用：" + f[i] + "元/（车·天）");
+            System.out.println("最大载人：" + m[i] + "人");
         }
-        FOR(INT S=3;S<8;S++)
+        for(int s=3;s<8;s++)
         {
-            SYSTEM.OUT.PRINTLN("序号:" + E[S]);
-            SYSTEM.OUT.PRINTLN("车辆名称:" + N[S]);
-            SYSTEM.OUT.PRINTLN("租车费用：" + F[S] + "元/（车·天）");
-            SYSTEM.OUT.PRINTLN("最大载货：" + M[S] + "吨");
+            System.out.println("序号:" + e[s]);
+            System.out.println("车辆名称:" + n[s]);
+            System.out.println("租车费用：" + f[s] + "元/（车·天）");
+            System.out.println("最大载货：" + m[s] + "吨");
         }
 
 
         //租车数量和天数确认
-        SCANNER SC = NEW SCANNER(SYSTEM.IN);
-        INT L = 8;//遍历循环8次
-        INT[] DAY = NEW INT[L];//租车天数
-        INT[] NUM = NEW INT[L];//租车数量
-        FOR(INT K=0;K<8;K++)
+        Scanner sc = new Scanner(System.in);
+        int l = 8;//遍历循环8次
+        int[] day = new int[l];//租车天数
+        int[] num = new int[l];//租车数量
+        for(int k=0;k<8;k++)
         {
-                SYSTEM.OUT.PRINTLN("请输入序号" + E[K] + "的租车数量：" );
-                NUM[K] = SC.NEXTINT();
-                IF(NUM[K]>=0)
+                System.out.println("请输入序号" + e[k] + "的租车数量：" );
+                num[k] = sc.nextInt();
+                if(num[k]>0)
                 {
-                        SYSTEM.OUT.PRINTLN("序号"+E[K]+"的租车天数为：");
-                        DAY[K] = SC.NEXTINT();
-                        SYSTEM.OUT.PRINTLN("请确认序号"+E[K]+"的租车天数：");
-                        INT DAY1 = SC.NEXTINT();
+                        System.out.println("序号"+e[k]+"的租车天数为：");
+                        day[k] = sc.nextInt();
+                        System.out.println("请确认序号"+e[k]+"的租车天数：");
+                        int day1 = sc.nextInt();
 
-                IF(DAY[K] == DAY1)
+                if(day[k] == day1)
                 {
-                        SYSTEM.OUT.PRINTLN("车辆序号为" + E[K] + "     " + "租车数量为：" + NUM[K] + "      " + "租车天数为：" + DAY[K]);
-                }ELSE
+                        System.out.println("车辆序号为" + e[k] + "     " + "租车数量为：" + num[k] + "      " + "租车天数为：" + day[k]);
+                }else
                 {
-                        SYSTEM.OUT.PRINTLN("您输入的天数不一样!");
+                        System.out.println("您输入的天数不一样!");
                 }
 
-                }ELSE
+                }else
                 {
-                        SYSTEM.OUT.PRINTLN("没有该车辆的序号！");
-                }
+                        System.out.println("没有该车辆的序号！");
+                }                       
         }
 
 
         //数据的最终结算与展示
-        INT SUMP = 0;//最大载人量
-        INT SUMH = 0;//最大载货量
-        INT SUMF = 0;//总体租车费用
-        INT SUMC = 0;//租用车辆总数
+        int sump = 0;//最大载人量
+        int sumh = 0;//最大载货量
+        int sumf = 0;//总体租车费用
+        int sumc = 0;//租用车辆总数
         //遍历数组内相加
-        FOR(INT X=0;X<NUM.LENGTH;X++)//INT Z:F[K];INT C:)
-        {
-                SUMC += NUM[X];
-                SUMP += M[X]*NUM[X];//数组相乘
-                SUMF += NUM[X]*DAY[X]*F[X];
-                IF(X>3)
+        for(int x=0;x<num.length;x++)//int z:f[k];int c:)
+        {       
+                sumc += num[x];
+                sump += m[x]*num[x];//数组相乘
+                sumf += num[x]*day[x]*f[x];
+                if(x>3)
                 {
-                     SUMH += M[X]*NUM[X];
+                     sumh += m[x]*num[x]; 
                 }
-
+                
         }
         //展示
-        SYSTEM.OUT.PRINTLN("总体租车的最大载人量"+SUMP);
-        SYSTEM.OUT.PRINTLN("总体租车的最大载货量"+SUMH);
-        SYSTEM.OUT.PRINTLN("总体租车费用"+SUMF);
-        SYSTEM.OUT.PRINTLN("租用车辆总数"+SUMC);
-        FOR(INT Y=0;Y<NUM.LENGTH;Y++)
+        System.out.println("总体租车的最大载人量"+sump);
+        System.out.println("总体租车的最大载货量"+sumh);
+        System.out.println("总体租车费用"+sumf);
+        System.out.println("租用车辆总数"+sumc);
+        for(int y=0;y<num.length;y++)
         {
-                IF(NUM[Y] != 0)
-                {
-                     SYSTEM.OUT.PRINTLN("车辆序号" + E[Y] + "租用数目为：" + NUM[Y] + "       " + "租用天数为：" + DAY[Y]);
-
-                }
+                System.out.println("车辆序号"+e[y]+"租用数目为："+num[y]);
         }
-        SYSTEM.EXIT(0);
+        System.exit(0);
     }  
 }
